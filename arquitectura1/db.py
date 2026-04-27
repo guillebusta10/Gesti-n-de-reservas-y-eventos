6c68249bcd0707602795cc03b34f3c628952038f
Guillermo Bustamante <64450610+guillebusta10@users.noreply.github.com>
2026-04-27 17:38:46 -0400

    avance

--- arquitectura1/db.py
+++ arquitectura1/db.py
@@ -1,13 +1,24 @@
 import psycopg
-from config import DB_HOST, DB_PORT, DB_NAME, DB_USER, DB_PASS, DB_SSLMODE
-
+import os
+try:
+    from config import DB_HOST, DB_PORT, DB_NAME, DB_USER, DB_PASS, DB_SSLMODE
+except ImportError:
+    DB_HOST = DB_PORT = DB_NAME = DB_USER = DB_PASS = None
+    DB_SSLMODE = "prefer"
 
 def obtener_conexion():
+    url_nube = os.environ.get('DATABASE_URL')
+    
+    if url_nube:
+        if url_nube.startswith("postgres://"):
+            url_nube = url_nube.replace("postgres://", "postgresql://", 1)
+        return psycopg.connect(url_nube)
+    
     return psycopg.connect(
-        host=DB_HOST,
-        port=DB_PORT,
-        dbname=DB_NAME,
-        user=DB_USER,
-        password=DB_PASS,
-        sslmode=DB_SSLMODE
+        host=os.environ.get('DB_HOST', DB_HOST),
+        port=os.environ.get('DB_PORT', DB_PORT),
+        dbname=os.environ.get('DB_NAME', DB_NAME),
+        user=os.environ.get('DB_USER', DB_USER),
+        password=os.environ.get('DB_PASS', DB_PASS),
+        sslmode=os.environ.get('DB_SSLMODE', DB_SSLMODE)
     )
